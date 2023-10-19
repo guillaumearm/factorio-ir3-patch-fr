0e7c52fcb66a04d3019a8ea1150ab8623d6201ba
Guillaume ARM <guillaume.arm@payfit.com>
2023-10-19 22:11:59 +0200

    chore: add package-zip.sh script

---
+++ cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BUILD_FOLDER=build
+
+rm -rf $BUILD_FOLDER
+echo "Cleaned \"$BUILD_FOLDER\" directory"
+
---
+++ package-zip.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VERSION=1.0.0
+BUILD_FOLDER=build
+MOD_DIRNAME=IR3_PatchFR_$VERSION
+MOD_ZIPNAME=$MOD_DIRNAME.zip
+
+rm -rf $BUILD_FOLDER
+mkdir -p $BUILD_FOLDER/$MOD_DIRNAME
+
+cp LICENSE $BUILD_FOLDER/$MOD_DIRNAME
+cp info.json $BUILD_FOLDER/$MOD_DIRNAME
+cp changelog.txt $BUILD_FOLDER/$MOD_DIRNAME
+cp -r locale $BUILD_FOLDER/$MOD_DIRNAME
+
+cd build
+zip -r $MOD_ZIPNAME $MOD_DIRNAME/
+cd - > /dev/null
+
+echo "Created package $MOD_DIRNAME.zip"
+
